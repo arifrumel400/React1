5fe2c554f7b6891d9ad199cbf2da6b9cd3e11506
arifrumel400 <arif.rumel400@gmail.com>
2021-05-30 12:54:22 +0600

    database set up

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/user')
+    .then(() => console.log('Db connected'))
+    .catch(error => console.log(error))
+    
 
 app.listen(5000, () => console.log('server started'));
